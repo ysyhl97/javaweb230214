66b53cd6216eb46a64fd2fbb942ae5660f2195d6
ysyhl97 <ttswbqz@gmail.com>
2023-03-01 10:12:40 +0800

    pro17modules中ClassPathXml中添加相应注释

--- pro17-fruit2.2-mvc-service-ioc/src/com/atguigu/myssm/io/impl/ClassPathXmlApplicationContext.java
+++ pro17-fruit2.2-mvc-service-ioc/src/com/atguigu/myssm/io/impl/ClassPathXmlApplicationContext.java
@@ -37,17 +37,27 @@ public class ClassPathXmlApplicationContext implements BeanFactory {
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             // 创建documentBuilder对象
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            // 创建document对象
             Document document = documentBuilder.parse(inputStream);
 
+            // 获取所有bean标签节点
             NodeList nodeList = document.getElementsByTagName("bean");
             for (int i = 0; i < nodeList.getLength(); i++) {
+                // 根据索引返回对应位置的node
                 Node node = nodeList.item(i);
+                // 如果node类型是element节点
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    // 将node转为元素节点
                     Element elementNode = (Element) node;
+                    // 获取bean标签中id属性值
                     String beanId = elementNode.getAttribute("id");
+                    // 获取bean标签中class属性值(也就是id对应的全类名)
                     String beanClassName = elementNode.getAttribute("class");
+                    // 创建该类的Class对象
                     Class<?> controllerBeanClass = Class.forName(beanClassName);
+                    // 根据Class对象创建该类的对象
                     Object controllerBeanObj = controllerBeanClass.newInstance();
+                    // 将该对象放到map集合中
                     beanMap.put(beanId, controllerBeanObj);
                 }
             }
@@ -56,18 +66,34 @@ public class ClassPathXmlApplicationContext implements BeanFactory {
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     Element elementNode = (Element) node;
                     String beanId = elementNode.getAttribute("id");
+                    // 获取当前元素节点的所有子节点
                     NodeList childNodes = elementNode.getChildNodes();
                     for (int j = 0; j < childNodes.getLength(); j++) {
+                        // 获取对应索引位置的子节点
                         Node childNode = childNodes.item(j);
+                        // 子节点的类型是元素节点 并且 子节点的节点名为property
                         if (childNode.getNodeType() == Node.ELEMENT_NODE && "property".equals(childNode.getNodeName())){
+                            // 将子节点转换元素节点
                             Element propertyElement = (Element) childNode;
+                            // 获取子元素节点中的name属性值
                             String propertyName = propertyElement.getAttribute("name");
+                            // 获取子元素节点中的ref属性值(String类型)
                             String propertyRef = propertyElement.getAttribute("ref");
+                            // 根据ref属性值获取对应的Object对象
+                            // 前面我们已经将所有bean标签对应的对象,放在beanMap中
+                            // 此处当然可以获取对象中属性值
                             Object refObj = beanMap.get(propertyRef);
+                            // 获取相应的bean对象
                             Object beanObj = beanMap.get(beanId);
+                            // 获取bean对象的Class对象
                             Class<?> beanClazz = beanObj.getClass();
+                            // 获取属性名对应的属性对象(此处为property标签中name属性对应的对象)
                             Field propertyField = beanClazz.getDeclaredField(propertyName);
+                            // 强制访问
                             propertyField.setAccessible(true);
+                            // set(obj,value) obj是需要修改字段的对象 value是需要修改字段对象的字段的修改后的新值
+                            // 这里的beanObj表明需要修改的是该bean标签下的property标签中name属性
+                            // refObj是name属性修改后的值
                             propertyField.set(beanObj,refObj);
                         }
                     }
